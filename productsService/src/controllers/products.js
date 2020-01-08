9e2674aacb25de5092db2aa2b4054b4788361f9c
Khalifa Gad <kkhalifa.gad@gmail.com>
2020-01-08 12:40:45 +0200

    connect routes with controllers

---
+++ productsService/src/controllers/products.js
@@ -0,0 +1,58 @@
+
+/**
+ * Post "api/v1/products/product" handler function 
+ * @param req: request param 
+ * @param res: response param
+ * @param next: express middleware function
+ */
+function addProduct(req, res, next) {
+
+}
+
+/**
+ * Get "api/v1/products" handler function 
+ * @param req: request param 
+ * @param res: response param
+ * @param next: express middleware function
+ */
+function getProducts(req, res, next) {
+
+}
+
+/**
+ * Get "api/v1/products/:id" handler function 
+ * @param req: request param 
+ * @param res: response param
+ * @param next: express middleware function
+ */
+function getProduct(req, res, next) {
+
+}
+
+/**
+ * Delete "api/v1/products/:id" handler function 
+ * @param req: request param 
+ * @param res: response param
+ * @param next: express middleware function
+ */
+function deleteProduct(req, res, next) {
+
+}
+
+/**
+ * Put "api/v1/products/:id" handler function 
+ * @param req: request param 
+ * @param res: response param
+ * @param next: express middleware function
+ */
+function updateProduct(req, res, next) {
+
+}
+
+export {
+    addProduct,
+    getProducts,
+    getProduct,
+    deleteProduct,
+    updateProduct
+}
