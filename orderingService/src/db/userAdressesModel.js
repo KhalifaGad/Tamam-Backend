32ef53d17cfd9171e1fa5877756c54abd196c563
Khalifa Gad <kkhalifa.gad@gmail.com>
2020-01-20 15:37:59 +0200

    user addresses modification

--- orderingService/src/db/userAdressesModel.js
+++ orderingService/src/db/userAdressesModel.js
@@ -1,33 +1,38 @@
 import mongoose from 'mongoose'
 
 let userAddressesSchema = mongoose.Schema({
-    lat: {
-        type: Number,
-        required: true
-    },
-    long: {
-        type: Number,
-        required: true
-    },
-    street: {
-        type: String,
-        required: true,
-    },
-    area: {
-        type: String,
-        required: true
-    },
-    city: {
-        type: String,
-        required: true
-    },
-    buildingNo: String,
-    floorNo: Number,
-    landmark: String,
-    addressNote: String,
     userId: {
         type: mongoose.Schema.Types.ObjectId,
         required: true
+    },
+    addresses: {
+        type: [{
+            lat: {
+                type: Number,
+                required: true
+            },
+            long: {
+                type: Number,
+                required: true
+            },
+            street: {
+                type: String,
+                required: true,
+            },
+            area: {
+                type: String,
+                required: true
+            },
+            city: {
+                type: String,
+                required: true
+            },
+            buildingNo: String,
+            floorNo: Number,
+            landmark: String,
+            addressNote: String
+        }],
+        index: 1
     }
 })
 
