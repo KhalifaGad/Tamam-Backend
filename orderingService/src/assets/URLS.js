74e7937b35c94ba45ea9f7ed6c88a2b8dd871e2b
Khalifa Gad <kkhalifa.gad@gmail.com>
2020-01-21 11:58:43 +0200

    partial commit to complete another tasks

---
+++ orderingService/src/assets/URLS.js
@@ -0,0 +1,7 @@
+const urls = {
+    users: {
+        BASE_URL: process.env.USERS_SERVICE_CONTAINER_NAME 
+        + ':'
+        + process.env.USERS_SERVICE_ACCESS_PORT
+    }
+}
--- orderingService/src/controllers/orders.js
+++ orderingService/src/controllers/orders.js
@@ -1,7 +1,18 @@
 import OrderModel from '../db/orderModel'
+import boom from '@hapi/boom'
 
 function makeOrder(req, res, next){
-    
+    // check the user authentication,
+    let auth = req.headers.authentication
+    if(!auth){
+        next(boom.forbidden('Authentication required!'))
+    }
+
+    // sending auth var to user authentication api
+
+    // fetch the product
+    // check for offer and fetch it if exist
+
 }
 
 function getUserOrders(req, res, next){
