eaa0b8364cffb3e2ee74a3dd147d1c1ad90eaada
KhalifaGad <kkhalifa.gad@gmail.com>
2020-01-26 11:35:18 +0200

    add time to tokensModel

--- usersService/src/db/tokensModel.js
+++ usersService/src/db/tokensModel.js
@@ -2,7 +2,11 @@ import mongoose from 'mongoose'
 
 let tokensSchema = new mongoose.Schema({
     token: String,
-    userId: mongoose.Schema.Types.ObjectId
+    userId: mongoose.Schema.Types.ObjectId,
+    issueDate:{
+        type: Date,
+        default: new Date()
+    } 
 })
 
 let TokensModel = mongoose.model('Tokens', tokensSchema)
