0ffc006012d65ef6f0e8e08bd4e5e7d73744fcde
Khalifa Gad <kkhalifa.gad@gmail.com>
2020-01-28 16:01:23 +0200

    testing purposes

--- orderingService/src/controllers/orders.js
+++ orderingService/src/controllers/orders.js
@@ -2,25 +2,37 @@ import OrderModel from '../db/orderModel'
 import boom from '@hapi/boom'
 import axios from 'axios'
 
-async function makeOrder(req, res, next){
+async function makeOrder(req, res, next) {
     // check the user authentication,
     let auth = req.headers.authentication
-    if(!auth){
+    if (!auth) {
         next(boom.forbidden('Authentication required!'))
     }
 
     // sending auth var to user authentication api
 
-    let userId = await axios.create({
-        baseURL: ''
+    let authResponse = await axios.create({
+        baseURL: 'http://users-service:3002/api/v1',
+        headers: {
+            authentication: auth
+        }
+    }).get('/auth/user/cardinalities').catch(err => {
+        next(boom.internal(err))
     })
 
+    console.log(authResponse)
+
+    if (!authResponse.isAuthenticated) {
+        next(boom.forbidden('Authentication required!'))
+    }
+
+    return res.send("ok")
     // fetch the product
     // check for offer and fetch it if exist
 
 }
 
-function getUserOrders(req, res, next){
+function getUserOrders(req, res, next) {
 
 }
 
