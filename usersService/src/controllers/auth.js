e752b917416eee120b185e86d19d4924e5bc7b5e
Khalifa Gad <kkhalifa.gad@gmail.com>
2020-02-09 15:53:15 +0200

    add user to get carinalites response

--- usersService/src/controllers/auth.js
+++ usersService/src/controllers/auth.js
@@ -24,12 +24,12 @@ async function authenticate(req, res, next) {
 
     if (!user.isVerified) {
         res.status(302).send({
-          isSuccessed: true,
-          data: {
-            user,
-            token: null
-          },
-          error: "User not verified!"
+            isSuccessed: true,
+            data: {
+                user,
+                token: null
+            },
+            error: "User not verified!"
         })
     }
     //tokenModel
@@ -44,7 +44,7 @@ async function authenticate(req, res, next) {
         console.log('saving token error, Error:' + err)
     })
 
-    if(user.lastActiveDevice !== device) {
+    if (user.lastActiveDevice !== device) {
         user.lastActiveDevice = device
         user.save().catch(err => {
             console.log('updatig user last active device, Error:' + err)
@@ -77,35 +77,42 @@ async function authenticate(req, res, next) {
 
 // check for the old tokens too for the same user Id
 async function getUserCardinalities(req, res, next) {
-  let docodingRes = decodeToken(req)
+    let isUserIncluded = req.params.inc || false,
+        user = null,
+        docodingRes = decodeToken(req)
 
-  if(!docodingRes.isAuthenticated) {
-      return next(boom.unauthorized('Auth not included or malformed'))
-  }
+    if (!docodingRes.isAuthenticated) {
+        return next(boom.unauthorized('Auth not included or malformed'))
+    }
 
-  let tokenModel = await TokensModel.findOne({
-      userId: docodingRes.userId
-  }).sort('issueDate')
+    let tokenModel = await TokensModel.findOne({
+        userId: docodingRes.userId
+    }).sort('issueDate')
 
-  if(tokenModel.token !== docodingRes.token){
-      return res.status(302).send({
-          isSuccessed: false,
-          data: {
-              link: '/api/v1/auth',
-              method: 'POST'
-          },
-          error: 'Re-login required!'
-      })
-  }
+    if (tokenModel.token !== docodingRes.token) {
+        return res.status(302).send({
+            isSuccessed: false,
+            data: {
+                link: '/api/v1/auth',
+                method: 'POST'
+            },
+            error: 'Re-login required!'
+        })
+    }
 
-  return res.status(200).send({
-      isSuccessed: true,
-      data: {
-          userId: docodingRes.userId,
-          role: docodingRes.role
-      },
-      error: null
-  })
+    if (isUserIncluded) {
+        user = await UserModel.findById(docodingRes.userId)
+    }
+
+    return res.status(200).send({
+        isSuccessed: true,
+        data: {
+            user,
+            userId: docodingRes.userId,
+            role: docodingRes.role
+        },
+        error: null
+    })
 
 }
 
