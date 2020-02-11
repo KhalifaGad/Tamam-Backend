6209899079dd9e867cde9e6339eeff2dbd778739
Khalifa Gad <kkhalifa.gad@gmail.com>
2020-02-11 11:00:31 +0200

    nn

--- productsService/src/controllers/products.js
+++ productsService/src/controllers/products.js
@@ -92,7 +92,7 @@ async function getProducts(req, res, next) {
         .then(docs => {
             docs = docs.map(product => {
                 product.isFav = favorites.indexOf(product._id.toString()) == -1 ?
-                    false : true
+                    Boolean(false) : Boolean(true)
                 product.name = product.name[retrevingLang]
                 product.description = product.description[retrevingLang]
                 product.keyImage = product.images[0] || ""
