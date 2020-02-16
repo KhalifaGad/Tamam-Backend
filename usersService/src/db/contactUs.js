a9c1ec90a9acc418df4dd8ea173af6a522c8e8dc
Khalifa Gad <kkhalifa.gad@gmail.com>
2020-02-16 11:36:03 +0200

    contact us model

---
+++ usersService/src/db/contactUs.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose'
+
+let contactUsSchema = mongoose.Schema({
+    userName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    phone: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    message: {
+        type: String,
+        required: true,
+        trim: true
+    }
+}, { versionKey: false })
+
+let ContactUsModel = mongoose.model('ContactUs', contactUsSchema)
+
+export { ContactUsModel }
