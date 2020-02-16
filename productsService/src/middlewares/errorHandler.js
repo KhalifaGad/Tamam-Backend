6d4da68aa1795f15ccdfd5bf12e9bbd4a4d8333a
Khalifa Gad <kkhalifa.gad@gmail.com>
2020-02-16 12:46:17 +0200

    change error handler for better

--- productsService/src/middlewares/errorHandler.js
+++ productsService/src/middlewares/errorHandler.js
@@ -4,13 +4,13 @@ function errorHandling(err, req, res, next){
     console.log(err)
     if(err.output.statusCode >= 500){
         console.log(err) // supposed to be in the error loging file
-        res.status(err.output.statusCode).send({
+        return res.status(err.output.statusCode).send({
             isSuccessed: false,
             data: null,
             error: "Its not you, its us, we are working on fixing it :("
         })
     }
-    res.status(err.output.statusCode).send({
+    return res.status(err.output.statusCode).send({
         isSuccessed: false,
         data: null,
         error: err.output.payload.message
