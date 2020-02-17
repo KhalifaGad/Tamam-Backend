96995e2018ede5abec4fc8c0b2032e44a0885870
KhalifaGad <kkhalifa.gad@gmail.com>
2020-02-17 17:10:46 +0200

    some changes

--- orderingService/src/controllers/orders.js
+++ orderingService/src/controllers/orders.js
@@ -1,46 +1,58 @@
-import OrderModel from '../db/models/order'
-import boom from '@hapi/boom'
-import axios from 'axios'
+import OrderModel from "../db/models/order";
+import boom from "@hapi/boom";
+import axios from "axios";
 
 async function makeOrder(req, res, next) {
-    // check the user authentication,
-    let auth = req.headers.authentication
-    if (!auth) {
-        next(boom.forbidden('Authentication required!'))
-    }
+  // check the user authentication,
+  let auth = req.headers.authentication;
+  if (!auth) {
+    next(boom.forbidden("Authentication required!"));
+  }
 
-    // sending auth var to user authentication api
-    let authResponse
+  // sending auth var to user authentication api
+  let authResponse;
 
-    try {
-        authResponse = await axios.create({
-            baseURL: 'http://users-service:3002/api/v1',
-            headers: {
-                authentication: auth
-            }
-        }).get('/auth/user/cardinalities')
-    } catch (err) {
-        return res.status(err.response.status)
-            .send(err.response.data)
-    }
-    
-    //authResponse.status, authResponse.data
-    console.log(authResponse.status)
-    if (!authResponse.isAuthenticated) {
-        next(boom.forbidden('Authentication required!'))
-    }
+  try {
+    authResponse = await axios
+      .create({
+        baseURL: "http://users-service:3002/api/v1",
+        headers: {
+          authentication: auth
+        }
+      })
+      .get("/auth/user/cardinalities");
+  } catch (err) {
+    return res.status(err.response.status).send(err.response.data);
+  }
 
-    return res.send("ok")
-    // fetch the product
-    // check for offer and fetch it if exist
+  //authResponse.status, authResponse.data
+  console.log(authResponse.status);
+  if (!authResponse.isAuthenticated) {
+    next(boom.forbidden("Authentication required!"));
+  }
 
+  return res.send("ok");
+  let product 
+  try {
+    product = await axios
+      .create({
+        baseURL: "http://products-service:3001/api/v1",
+        headers: {
+          authentication: auth
+        }
+      })
+      .get("/auth/user/cardinalities");
+  } catch (err) {
+    return res.status(err.response.status).send(err.response.data);
+  }
+
+  // fetch the product
+  // check for offer and fetch it if exist
 }
 
-function getUserOrders(req, res, next) {
+function getUserOrders(req, res, next) {}
 
-}
-
-export { makeOrder, getUserOrders }
+export { makeOrder, getUserOrders };
 
 /*
 
