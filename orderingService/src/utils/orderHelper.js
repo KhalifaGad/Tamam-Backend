546b23f8d5e7e8b791d8f4ee2180e53982979384
KhalifaGad <kkhalifa.gad@gmail.com>
2020-02-19 17:01:25 +0200

    testing

--- orderingService/src/utils/orderHelper.js
+++ orderingService/src/utils/orderHelper.js
@@ -1,8 +1,43 @@
 async function prepareOrder(products, reqArray) {
-  let orderTotal = 0,
-    tax = 5, // supposed to be retrieved from DB
-    estimatedTime = 0,
-    preparedOrderArr = await reqArray.filter(obj => {
+  let productsBySeller = grouptBySeller(products);
+  let orders = [];
+
+  for (let seller in productsBySeller) {
+    let orderTotal = 0,
+      tax = 5, // supposed to be retrieved from DB
+      estimatedTime = 0;
+
+    orders.push({
+      preparedOrderArr: reqArray.filter(obj => {
+        try {
+          let product = productsBySeller[seller].filter(
+            product => product._id + "" == obj.productId + ""
+          )[0];
+          obj.price = product.price;
+          obj.total = product.price;
+          if (product.estimatedDeliveryTime > estimatedTime) {
+            estimatedTime = product.estimatedDeliveryTime;
+          }
+          if (product.offerId) {
+            obj.offerId = product.offerId._id;
+            obj.total = obj.price * (1 - product.offerId.discountRatio / 100);
+          }
+          orderTotal += obj.total;
+          return obj;
+        } catch (err) {
+          console.log(err);
+          return false;
+        }
+      }),
+      orderTotal,
+      tax,
+      estimatedTime,
+      grandTotal: orderTotal * (1 + tax / 100)
+    });
+  }
+  return orders
+
+  let preparedOrderArr = await reqArray.filter(obj => {
       try {
         let product = products.filter(
           product => product._id + "" == obj.productId + ""
@@ -31,6 +66,18 @@ async function prepareOrder(products, reqArray) {
     tax,
     estimatedTime
   };
+}
+
+function grouptBySeller(products) {
+  let sellersProducts = {};
+  for (let i = 0; i < products.length; i++) {
+    if (sellersProducts[products[i].seller] == undefined) {
+      sellersProducts[products[i].seller] = [products[i]];
+    } else {
+      sellersProducts[products[i].seller].psuh(products[i]);
+    }
+  }
+  return sellersProducts;
 }
 
 export { prepareOrder };
