8d5c66307af28d910329e6f60905b9a2a10be148
KhalifaGad <kkhalifa.gad@gmail.com>
2020-02-19 15:16:20 +0200

    some validations

--- orderingService/src/utils/orderHelper.js
+++ orderingService/src/utils/orderHelper.js
@@ -2,21 +2,26 @@ async function prepareOrder(products, reqArray) {
   let orderTotal = 0,
     tax = 5, // supposed to be retrieved from DB
     estimatedTime = 0,
-    preparedOrderArr = await reqArray.map(obj => {
-      let product = products.filter(
-        product => product._id + "" == obj.productId + ""
-      )[0];
-      obj.price = product.price;
-      obj.total = product.price;
-      if(product.estimatedDeliveryTime > estimatedTime){
-        estimatedTime = product.estimatedDeliveryTime
+    preparedOrderArr = await reqArray.filter(obj => {
+      try {
+        let product = products.filter(
+          product => product._id + "" == obj.productId + ""
+        )[0];
+        obj.price = product.price;
+        obj.total = product.price;
+        if (product.estimatedDeliveryTime > estimatedTime) {
+          estimatedTime = product.estimatedDeliveryTime;
+        }
+        if (product.offerId) {
+          obj.offerId = product.offerId._id;
+          obj.total = obj.price * (1 - product.offerId.discountRatio / 100);
+        }
+        orderTotal += obj.total;
+        return obj;
+      } catch (err) {
+        console.log(err);
+        return false;
       }
-      if (product.offerId) {
-        obj.offerId = product.offerId._id;
-        obj.total = obj.price * (1 - product.offerId.discountRatio / 100);
-      }
-      orderTotal += obj.total;
-      return obj;
     }),
     grandTotal = orderTotal * (1 + tax / 100);
   return {
