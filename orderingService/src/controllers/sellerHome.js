aee37bef7ab489dc7f23d088922b126e0755a2ca
KhalifaGad <kkhalifa.gad@gmail.com>
2020-02-20 15:33:02 +0200

    add newest orders

--- orderingService/src/controllers/sellerHome.js
+++ orderingService/src/controllers/sellerHome.js
@@ -1,27 +1,29 @@
-import { ordersModule } from "../db/modules/orders"
+import { ordersModule } from "../db/modules/orders";
 
-async function sellerHome(req, res, next){
-    let sellerId = req.body.sellerId
+async function sellerHome(req, res, next) {
+  let sellerId = req.body.sellerId;
 
-    let orders = await ordersModule.getSellerOrders(sellerId)
+  let orders = await ordersModule.getSellerOrders(sellerId);
 
-    let totalOrdersCount = orders.length,
-        paidOrdersRatio = (orders.filter(order => order.isPaid).length / totalOrdersCount) * 100,
-        customersCount = new Set(orders.map(order => order.userId)).size,
-        completedOrdersCount = orders.filter(order => order.State == 'TAMAM').length,
-        pendingOrdersCount = orders.filter(order => order.State == 'PENDING').length,
-        rejectedOrdersCount = orders.filter(order => order.State == 'REFUSED').length
+  let totalOrdersCount = orders.length,
+    newestOrders = orders.filter(order => order.State == "PENDING"),
+    paidOrdersRatio =
+      (orders.filter(order => order.isPaid).length / totalOrdersCount) * 100,
+    customersCount = new Set(orders.map(order => order.userId)).size,
+    completedOrdersCount = orders.filter(order => order.State == "TAMAM")
+      .length,
+    pendingOrdersCount = newestOrders.length,
+    rejectedOrdersCount = orders.filter(order => order.State == "REFUSED")
+      .length;
 
-    console.log(orders)
-    console.log(totalOrdersCount)
-    console.log(paidOrdersRatio)
-    console.log(customersCount)
-    console.log(completedOrdersCount)
-    console.log(pendingOrdersCount)
-    console.log(rejectedOrdersCount)
+  console.log(totalOrdersCount);
+  console.log(paidOrdersRatio);
+  console.log(customersCount);
+  console.log(completedOrdersCount);
+  console.log(pendingOrdersCount);
+  console.log(rejectedOrdersCount);
 
-
-    res.send('ok')
+  res.send("ok");
 }
 
-export { sellerHome }
+export { sellerHome };
