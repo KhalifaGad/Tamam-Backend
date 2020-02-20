e2c8ac5a481656028bb1cdbc3d509507ec1d89d4
KhalifaGad <kkhalifa.gad@gmail.com>
2020-02-20 10:41:39 +0200

    adding seller and admin acounts

--- usersService/src/middlewares/validationHandler.js
+++ usersService/src/middlewares/validationHandler.js
@@ -1,103 +1,115 @@
-import boom from '@hapi/boom'
+import boom from "@hapi/boom";
 import {
-    addUserValidationSchema,
-    addCountryValidationSchema,
-    verifyUserSchema,
-    resendVerificationSchema,
-    authenticationSchema,
-    idQueryParamVS,
-    addMessageVS,
-    editProductVS
-} from '../utils/validationSchemas'
+  addUserValidationSchema,
+  addCountryValidationSchema,
+  verifyUserSchema,
+  resendVerificationSchema,
+  authenticationSchema,
+  idQueryParamVS,
+  addMessageVS,
+  editProductVS
+} from "../utils/validationSchemas";
 
 function addUserValidation(req, res, next) {
-    const { error } = addUserValidationSchema.validate(req.body)
+  const { error } = addUserValidationSchema.validate(req.body);
 
-    if (error) {
-        next(boom.badData(error.details[0].message))
-    }
+  if (error) {
+    next(boom.badData(error.details[0].message));
+  }
 
-    next()
-
+  next();
 }
 
 function addCountryValidation(req, res, next) {
-    const { error } = addCountryValidationSchema.validate(req.body)
+  const { error } = addCountryValidationSchema.validate(req.body);
 
-    if (error) {
-        next(boom.badData(error.details[0].message))
-    }
+  if (error) {
+    next(boom.badData(error.details[0].message));
+  }
 
-    next()
+  next();
 }
 
 function verifyUserMiddleware(req, res, next) {
-    const { error } = verifyUserSchema.validate(req.body)
+  const { error } = verifyUserSchema.validate(req.body);
 
-    if (error) {
-        next(boom.badData(error.details[0].message))
-    }
+  if (error) {
+    next(boom.badData(error.details[0].message));
+  }
 
-    next()
+  next();
 }
 
 function rsndVrfcMiddleware(req, res, next) {
-    const { error } = resendVerificationSchema.validate(req.body)
+  const { error } = resendVerificationSchema.validate(req.body);
 
-    if (error) {
-        next(boom.badData(error.details[0].message))
-    }
+  if (error) {
+    next(boom.badData(error.details[0].message));
+  }
 
-    next()
+  next();
 }
 
 function loginValidation(req, res, next) {
-    const { error } = authenticationSchema.validate(req.body)
+  const { error } = authenticationSchema.validate(req.body);
 
-    if (error) {
-        next(next(boom.badData(error.details[0].message)))
-    }
+  if (error) {
+    next(next(boom.badData(error.details[0].message)));
+  }
 
-    next()
+  next();
 }
 
 function addMessageVM(req, res, next) {
-    const { error } = addMessageVS.validate(req.body)
+  const { error } = addMessageVS.validate(req.body);
 
-    if (error) {
-        next(next(boom.badData(error.details[0].message)))
-    }
+  if (error) {
+    next(next(boom.badData(error.details[0].message)));
+  }
 
-    next()
+  next();
 }
 
 function editProductVM(req, res, next) {
-    const { error } = editProductVS.validate(req.body)
+  const { error } = editProductVS.validate(req.body);
 
-    if (error) {
-        next(next(boom.badData(error.details[0].message)))
-    }
+  if (error) {
+    next(next(boom.badData(error.details[0].message)));
+  }
 
-    next()
+  next();
 }
 
 function idQueryParamVM(req, res, next) {
-    const { error } = idQueryParamVS.validate(req.params)
+  const { error } = idQueryParamVS.validate(req.params);
 
-    if (error) {
-        next(next(boom.badData(error.details[0].message)))
-    }
+  if (error) {
+    next(next(boom.badData(error.details[0].message)));
+  }
 
-    next()
+  next();
+}
+
+function roleValidation(req, res, next) {
+  const { role } = req.body;
+
+  if (role == "ADMIN" || role == "SELLER") {
+    let auth = req.headers.authentication;
+    if (auth != process.env.KHALIFAZ_SECRET)
+      return next(boom.unauthorized("You are not allowed for this action!"));
+  }
+
+  next();
 }
 
 export {
-    addUserValidation,
-    addCountryValidation,
-    verifyUserMiddleware,
-    rsndVrfcMiddleware,
-    loginValidation,
-    idQueryParamVM,
-    addMessageVM,
-    editProductVM
-}
+  addUserValidation,
+  addCountryValidation,
+  verifyUserMiddleware,
+  rsndVrfcMiddleware,
+  loginValidation,
+  idQueryParamVM,
+  addMessageVM,
+  editProductVM,
+  roleValidation
+};
--- usersService/src/routes/users/index.js
+++ usersService/src/routes/users/index.js
@@ -1,60 +1,62 @@
-import express from 'express'
+import express from "express";
 import {
-    addUser,
-    getUsers,
-    updateUser,
-    getUser,
-    deleteUser,
-    verifyUser,
-    resendVerification,
-    editUserFavs,
-    getUserFavs
-} from '../../controllers/users'
+  addUser,
+  getUsers,
+  updateUser,
+  getUser,
+  deleteUser,
+  verifyUser,
+  resendVerification,
+  editUserFavs,
+  getUserFavs
+} from "../../controllers/users";
 import {
-    addUserValidation,
-    verifyUserMiddleware,
-    rsndVrfcMiddleware,
-    idQueryParamVM,
-    editProductVM
-} from '../../middlewares/validationHandler'
-import multer from 'multer'
-import path from 'path'
-import { isIdsEquivalent } from '../../middlewares/customerAuthorization'
+  addUserValidation,
+  verifyUserMiddleware,
+  rsndVrfcMiddleware,
+  idQueryParamVM,
+  editProductVM
+} from "../../middlewares/validationHandler";
+import multer from "multer";
+import path from "path";
+import { isIdsEquivalent } from "../../middlewares/customerAuthorization";
 
-
-const usersRouter = express.Router()
+const usersRouter = express.Router();
 
 let storage = multer.diskStorage({
-    destination: 'userImages/',
-    filename: function (req, file, cb) {
-        cb(null, file.fieldname + '-' +
-            Date.now() +
-            path.extname(file.originalname))
+    destination: "userImages/",
+    filename: function(req, file, cb) {
+      cb(
+        null,
+        file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+      );
     }
-}),
-    upload = multer({
-        storage
-    })
+  }),
+  upload = multer({
+    storage
+  });
 
 // api/v1/users
-usersRouter.route('/')
-    .post(upload.single('userPic'), addUserValidation, addUser)
-    .get(getUsers)
+usersRouter
+  .route("/")
+  .post(upload.single("userPic"), addUserValidation, roleValidation, addUser)
+  .get(getUsers);
 
-usersRouter.route('/:id')
-    .put(idQueryParamVM, isIdsEquivalent, editProductVM, updateUser)
-    .get(idQueryParamVM, getUser)
-    .delete(idQueryParamVM, isIdsEquivalent, deleteUser)
+usersRouter
+  .route("/:id")
+  .put(idQueryParamVM, isIdsEquivalent, editProductVM, updateUser)
+  .get(idQueryParamVM, getUser)
+  .delete(idQueryParamVM, isIdsEquivalent, deleteUser);
 
 // api/v1/users/:id/favorites
 /* usersRouter.route('/:id/favorites')
     .put(idQueryParamVM, isIdsEquivalent, editUserFavs)
     .get(idQueryParamVM, isIdsEquivalent, getUserFavs) */
 
-usersRouter.route('/verification')
-    .post(verifyUserMiddleware, verifyUser)
+usersRouter.route("/verification").post(verifyUserMiddleware, verifyUser);
 
-usersRouter.route('/verification/resend')
-    .post(rsndVrfcMiddleware, resendVerification)
+usersRouter
+  .route("/verification/resend")
+  .post(rsndVrfcMiddleware, resendVerification);
 
-export { usersRouter }
+export { usersRouter };
