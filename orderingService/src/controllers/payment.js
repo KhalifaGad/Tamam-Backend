9f50e3ceff35ff0b9a39abcc7e41460124aa0cea
KhalifaGad <kkhalifa.gad@gmail.com>
2020-02-24 14:17:48 +0200

    modifications

--- orderingService/src/controllers/payment.js
+++ orderingService/src/controllers/payment.js
@@ -3,27 +3,26 @@ import boom from "@hapi/boom";
 import { ordersModule } from "../db/modules/orders";
 import { modifyProductsGroup } from "../utils/productsHelper";
 
-async function CODPayment(req, res, next){
-    let {
+async function CODPayment(req, res, next) {
+    let { userId, ordersIds } = req.body;
+  
+    await ordersIds.forEach(async orderId => {
+      let payment = await paymentModule.addPayment({
         userId,
-        orderId
-    } = req.body
-
-    let payment = await paymentModule.addPayment({userId, orderId, isConfirmed: true})
-    if(!payment) return next(boom.internal('Failer in adding payment'))
-
-    let order = await ordersModule.confirmOrder(orderId, payment._id)
-    modifyProductsGroup(order.products)
-    
+        orderId,
+        isConfirmed: true
+      });
+      if (!payment) return next(boom.internal("Failer in adding payment"));
+      let order = await ordersModule.confirmOrder(orderId, payment._id);
+      modifyProductsGroup(order.products);
+    });
+  
     res.status(201).send({
-        isSuccessed: true,
-        data: payment,
-        error: null
-    })
-
-
-
-}
+      isSuccessed: true,
+      data: payment,
+      error: null
+    });
+  }
 
 function sadadPayment(req, res, next){
 
