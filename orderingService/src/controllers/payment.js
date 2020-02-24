6bf146a6ff1761d4f17a7707374fad5b17b27823
KhalifaGad <kkhalifa.gad@gmail.com>
2020-02-24 14:39:53 +0200

    FB

--- orderingService/src/controllers/payment.js
+++ orderingService/src/controllers/payment.js
@@ -1,46 +1,35 @@
-import { paymentModule } from "../db/modules/payment"
+import { paymentModule } from "../db/modules/payment";
 import boom from "@hapi/boom";
 import { ordersModule } from "../db/modules/orders";
 import { modifyProductsGroup } from "../utils/productsHelper";
 
 async function CODPayment(req, res, next) {
-    let { userId, ordersIds } = req.body;
-    let payments = []
-  
-    await ordersIds.forEach(async orderId => {
-      let payment = await paymentModule.addPayment({
-        userId,
-        orderId,
-        isConfirmed: true
-      });
-      if (!payment) return next(boom.internal("Failer in adding payment"));
-      let order = await ordersModule.confirmOrder(orderId, payment._id);
-      modifyProductsGroup(order.products);
-      payments.push(payment)
+  let { userId, ordersIds } = req.body;
+  let payments = [];
+
+  for (orderId of ordersIds) {
+    let payment = await paymentModule.addPayment({
+      userId,
+      orderId,
+      isConfirmed: true
     });
-  
-    res.status(201).send({
-      isSuccessed: true,
-      data: payments,
-      error: null
-    });
+    if (!payment) return next(boom.internal("Failer in adding payment"));
+    let order = await ordersModule.confirmOrder(orderId, payment._id);
+    modifyProductsGroup(order.products);
+    payments.push(payment);
   }
 
-function sadadPayment(req, res, next){
-
+  res.status(201).send({
+    isSuccessed: true,
+    data: payments,
+    error: null
+  });
 }
 
-function onlinePayment(req, res, next){
+function sadadPayment(req, res, next) {}
 
-}
+function onlinePayment(req, res, next) {}
 
-function bankTransfer(req, res, next){
+function bankTransfer(req, res, next) {}
 
-}
-
-export {
-    CODPayment,
-    sadadPayment,
-    onlinePayment,
-    bankTransfer
-}
+export { CODPayment, sadadPayment, onlinePayment, bankTransfer };
