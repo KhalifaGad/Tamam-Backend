22661799b580ab3abf610710195b8bd8213d7ac2
KhalifaGad <kkhalifa.gad@gmail.com>
2020-02-25 15:01:52 +0200

    modifications

--- orderingService/src/routes/finance/index.js
+++ orderingService/src/routes/finance/index.js
@@ -6,10 +6,21 @@ import {
 } from "../../controllers/paymentTypes";
 import multer from "multer";
 import path from "path";
-import { addPaymentTypeVM, CODPaymentVM } from "../../middlewares/validationHandler";
+import {
+  addPaymentTypeVM,
+  paymentVM
+} from "../../middlewares/validationHandler";
 import { getSeller, getCustomer } from "../../middlewares/authenticator";
-import { CODPayment, sadadPayment, onlinePayment, bankTransfer } from "../../controllers/payment";
-import { checkPaymentAmount, checkOrders } from "../../middlewares/paymentHelper";
+import {
+  sadadPayment,
+  onlinePayment,
+  bankTransfer,
+  paymentProcess
+} from "../../controllers/payment";
+import {
+  checkPaymentAmount,
+  checkOrders
+} from "../../middlewares/paymentHelper";
 
 const financeRouter = Router();
 
@@ -29,12 +40,21 @@ let storage = multer.diskStorage({
 financeRouter
   .route("/payment-types")
   .get(getPaymentTypes)
-  .post(upload.single("paymentIcon"), addPaymentTypeVM, getSeller, addPaymentType)
+  .post(
+    upload.single("paymentIcon"),
+    addPaymentTypeVM,
+    getSeller,
+    addPaymentType
+  )
   .put(editPaymentType);
 
-financeRouter.route("/payment/COD").post(CODPaymentVM, getCustomer, checkOrders, CODPayment);
+financeRouter
+  .route("/payment")
+  .post(paymentVM, getCustomer, checkOrders, paymentProcess);
 financeRouter.route("/payment/sadad").post(getCustomer, sadadPayment);
-financeRouter.route("/payment/online").post(checkPaymentAmount, getCustomer, onlinePayment);
+financeRouter
+  .route("/payment/online")
+  .post(checkPaymentAmount, getCustomer, onlinePayment);
 financeRouter.route("/payment/bank-transfer").post(getCustomer, bankTransfer);
 
 export { financeRouter };
--- orderingService/src/utils/validationSchemas/payment.js
+++ orderingService/src/utils/validationSchemas/payment.js
@@ -1,10 +1,11 @@
 import Joi from '@hapi/joi'
 import { isMongooseId } from '../validationSchemasHelper'
 
-let CODPaymentVS = Joi.object({
+let paymentVS = Joi.object({
     ordersIds: Joi.array().items(
       Joi.custom(isMongooseId, "custom validation").required()
-    )
+    ),
+    paymentTypeId: Joi.custom(isMongooseId, "custom validation").required()
   });
 
-export { CODPaymentVS }
+export { paymentVS }
