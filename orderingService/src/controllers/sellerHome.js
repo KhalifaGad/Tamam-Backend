e7b9c2fb2e04f94c1f1de8c8dad8839e256572f2
KhalifaGad <kkhalifa.gad@gmail.com>
2020-02-27 13:13:33 +0200

    requestLowQuantityProducts

--- orderingService/src/controllers/sellerHome.js
+++ orderingService/src/controllers/sellerHome.js
@@ -1,14 +1,21 @@
 import { ordersModule } from "../db/modules/orders";
+import { getLowQuantityProducts } from "../utils/productsHelper";
 
 async function sellerHome(req, res, next) {
-  let sellerId = req.body.sellerId;
-
+  let sellerId = req.body.sellerId,
+    lang = req.query.lang;
   let orders = await ordersModule.getSellerOrders(sellerId);
+  let lowQuantityProducts = await getLowQuantityProducts(
+    req.headers.authentication,
+    sellerId,
+    lang
+  );
 
   let totalOrdersCount = orders.length,
     newestOrders = orders.filter(order => order.state == "SELLER PENDING"),
     paidOrdersRatio =
-      (orders.filter(order => order.isPaid).length / totalOrdersCount) * 100 || 0,
+      (orders.filter(order => order.isPaid).length / totalOrdersCount) * 100 ||
+      0,
     customersCount = new Set(orders.map(order => order.userId)).size,
     completedOrdersCount = orders.filter(order => order.state == "TAMAM")
       .length,
@@ -16,9 +23,11 @@ async function sellerHome(req, res, next) {
     rejectedOrdersCount = orders.filter(order => order.state == "REFUSED")
       .length;
 
+  console.log(lowQuantityProducts)
+
   res.status(200).send({
     isSuccessed: true,
-    data:{
+    data: {
       totalOrdersCount,
       paidOrdersRatio,
       customersCount,
@@ -28,7 +37,7 @@ async function sellerHome(req, res, next) {
       newestOrders
     },
     error: null
-  })
+  });
 }
 
 export { sellerHome };
